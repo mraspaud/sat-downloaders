04532405a855be874e00027da413a312b4595ea2
Martin Raspaud <martin.raspaud@smhi.se>
2022-08-25 13:34:36 +0200

    Raise for status when querying from dhus

--- sat_downloaders/dhus.py
+++ sat_downloaders/dhus.py
@@ -66,13 +66,12 @@ class DHUSDownloader(Downloader):
         logger.info(f"At {datetime.utcnow()}, requesting files over the baltic sea from {self.name}.")
         url = self.server + f"/search?q={' AND '.join(self.query_args)}&rows=100&start=0"
         res = requests.get(url)
-
+        res.raise_for_status()
         d = feedparser.parse(res.text)
         try:
             logger.debug(str(d.feed.title))
         except AttributeError:
-            logger.error("Can't get data from the hub")
-            return []
+            raise IOError("Can't get data from the hub")
 
         entries = [DHUSEntry(entry, self.auth, **self.entry_patterns) for entry in d.entries]
         self._check_entries(entries)
